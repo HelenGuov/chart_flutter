2d695b047e822f17a9104c18251073eda5a223c8
Helen Guov <helen.guov@croplogic.com>
2017-10-18 14:34:48 +1300

    can draw lines

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:ui' show lerpDouble;
+import 'dart:ui';
+import 'dart:typed_data';
 
 void main() {
   runApp(new MyApp());
@@ -69,11 +71,20 @@ class ChartPageState extends State<ChartPage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Center(
-        child: new CustomPaint(
+      body: new Container(
+        alignment: FractionalOffset.bottomLeft,
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children:  [
+            new CustomPaint(
+                size: new Size(200.0, 100.0),
+                painter: new BarChartPainter(currentHeight)
+          )],
+          ),
+    /*    child: new CustomPaint(
           size: new Size(200.0, 100.0),
           painter: new BarChartPainter(currentHeight),
-        ),
+        ),*/
       ),
       floatingActionButton: new FloatingActionButton(
         child: new Icon(Icons.refresh),
@@ -94,17 +105,54 @@ class BarChartPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final paint = new Paint()
       ..color = Colors.blue[400]
+      ..strokeWidth = 7.0
+      ..strokeCap = StrokeCap.round
+       ..style = PaintingStyle.fill;
+
+    double h = size.height;
+    debugPrint("size.height $h, barheight: $barHeight");
+/*
+    canvas.drawRect(new Rect.fromLTWH(0.0, 20.0, 30.0, 80.0), paint);
+*/
+
+/*
+    canvas.drawRect(new Rect.fromPoints(new Offset(0.0, 20.0), new Offset(5.0, 20.0)), paint);
+*/
+   /* var pointListof = [new Offset(51.0, -50.0), new Offset(100.0, -100.0), new Offset(100.0, -100.0), new Offset(150.0, -50.0),
+            new Offset(150.0, -50.0), new Offset(200.0, -100.0)];*/
+
+    var pointList = new Float32List.fromList(
+        [50.0, -50.0,
+        100.0, -100.0,
+        100.0, -100.0,
+        150.0, -50.0,
+        150.0, -50.0,
+        200.0, -100.0]);
+    canvas.drawRawPoints(PointMode.lines,  pointList, paint);
+
+    final paint2 = new Paint()
+      ..color = Colors.red[400]
+      ..strokeWidth = 7.0
+      ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.fill;
-    canvas.drawRect(
-      new Rect.fromLTWH(
-        (size.width - barWidth) / 2.0,
-        size.height - barHeight,
-        barWidth,
-        barHeight,
-      ),
-      paint,
-    );
+
+    var pointList2 = new Float32List.fromList(
+        [70.0, -50.0,
+        170.0, -100.0,
+        170.0, -100.0,
+        250.0, -50.0,
+        250.0, -50.0,
+        300.0, -100.0]);
+
+    canvas.drawRawPoints(PointMode.lines, pointList2, paint2);
+
+
+    /*canvas.drawCircle(new Offset(2.0, 5.0), 10.0, paint);
+    canvas.drawCircle(new Offset(50.0, 100.0), 10.0, paint);
+    canvas.drawLine(new Offset(2.0, 5.0), new Offset(50.0, 100.0), paint);
+*/
   }
+
 
   @override
   bool shouldRepaint(BarChartPainter old) => barHeight != old.barHeight;
