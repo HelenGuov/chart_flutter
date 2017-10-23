f04400d70ba1737aee39dc804db8f85979c7f054
Helen Guov <helen.guov@croplogic.com>
2017-10-24 12:32:30 +1300

    added axis labels

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:ui' show lerpDouble;
 import 'dart:ui';
+import 'dart:ui' show TextStyle;
 import 'dart:typed_data';
 
 void main() {
@@ -72,7 +73,7 @@ class ChartPageState extends State<ChartPage> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Container(
-        padding: new EdgeInsets.all(20.0),
+        padding: new EdgeInsets.fromLTRB(200.0, 50.0, 50.0, 50.0),
         alignment: FractionalOffset.bottomLeft,
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.end,
@@ -161,7 +162,7 @@ class BarChartPainter extends CustomPainter {
     canvas.drawLine(new Offset(0.0, 0.0), new Offset(0.0, -500.0), paintAx);
 
     //scales on x-axis
-    double xLen = 20.0;
+    double xLen = 10.0;
     var pointAxis = new Float32List.fromList([
       0.0, -50.0,
       xLen, -50.0,
@@ -170,6 +171,31 @@ class BarChartPainter extends CustomPainter {
     ]);
     canvas.drawRawPoints(PointMode.lines, pointAxis, paintAx);
 
+    //draw text
+    var tp = new TextPainter(
+        text: new TextSpan(
+          text: "100",
+          style: new TextStyle(color: Colors.black, fontSize: 15.0)
+        ),
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.left,
+    );
+    tp.layout();
+    tp.paint(canvas, new Offset(-40.0, -55.0));
+
+    var tp2 = new TextPainter(
+      text: new TextSpan(
+        text: "1st foot",
+        style: new TextStyle(color: Colors.black, fontSize: 20.0)
+      ),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.left
+    );
+    tp2.layout();
+    tp2.paint(canvas, new Offset(-100.0, -250.0));
+
+
+    //
     /*canvas.drawCircle(new Offset(2.0, 5.0), 10.0, paint);
     canvas.drawCircle(new Offset(50.0, 100.0), 10.0, paint);
     canvas.drawLine(new Offset(2.0, 5.0), new Offset(50.0, 100.0), paint);
